7fa8e2115b64ba5a0cd0e44eec0db6eefc484cab
ste ste <steelst0@gmail.com>
2018-07-30 20:56:46 -0700

    RemoveAt detecting values at front, middle, and end of list

--- DoublyLinkedList.cs
+++ DoublyLinkedList.cs
@@ -27,5 +27,56 @@ public class DoublyLinkedList
         }
     }
 
-
+    public bool Remove(int value)
+    {
+        DllNode runner = Head;
+        ///Edge case for if our list is empty
+        if(this.Head == null)
+        {
+            return false;
+        }
+        ////If a list with a single node contains our value
+        if(runner.Next == null)
+        {
+            if(runner.Value == value)
+            {
+                this.Head = null;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        ////If the list has > 1 nodes begin running through the list's nodes
+        else
+        {
+            while(runner.Next != null)
+            {
+                ////This handles every other possible node position.
+                if(runner.Value == value)
+                {
+                    Console.WriteLine("EVERY OTHER POS");
+                    return true;
+                }
+                runner = runner.Next;
+            }
+            ////This handles if the node we want is on the end of the list
+            if(runner.Next == null)
+            {
+                if(runner.Value == value)
+                {
+                    Console.WriteLine("END POS");
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+                
+            }
+            return false;
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -10,6 +10,14 @@ namespace Doubly_Linked_Lists
             DoublyLinkedList list1 = new DoublyLinkedList();
             list1.Add(7);
             list1.Add(12);
+            list1.Add(15);
+            list1.Add(20);
+            list1.Add(25);
+            Console.WriteLine(list1.Remove(2));
+            Console.WriteLine(list1.Remove(7));
+            Console.WriteLine(list1.Remove(20));
+            Console.WriteLine(list1.Remove(25));
+            Console.WriteLine(list1.Remove(30));
         }
     }
 }
